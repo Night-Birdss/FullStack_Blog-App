1e61e2e2a5b129be5f869510ca97eeb9cc2ab00d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-11 00:58:08 +0300

    login ve login form sayfası oluşturuldu formik ve yup dahil edildi ayarlamaları yapıldı

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Avatar, Box, Container, Grid, Link, Typography } from "@mui/material";
+import React from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import LoginForm from "../components/auth/LoginForm";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        alignItems="center"
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "green",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <PersonIcon />
+          </Avatar>
+          <Typography variant="h4" align="center" mb={4} color="green">
+            Login
+          </Typography>
+          //!LOGİN FORM
+          <LoginForm />
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/register">Do you have not an account?</Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
