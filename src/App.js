29b74304be013fecf0846dd9807dd600f4294829
asimov <seyranmustafa4@gmail.com>
2024-10-11 21:16:14 +0300

    about and newblog page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+// import NewBlog from "./pages/NewBlog";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+import About from "./pages/About";
 
 function App() {
-  return <div className="App">App</div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: purple[500],
+      },
+      secondary: {
+        main: "#f44336",
+      },
+    },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* <NewBlog /> */}
+        <About/>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
