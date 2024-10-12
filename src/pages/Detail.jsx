124cb35cea679bb6d46c4074902000c4d41a82ff
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-13 01:16:37 +0300

    detail sayfasına card yapısı oluşturuldu ve tarih zaman biçimleri ayarlandı datadan veriler çekildi

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
 
 const Detail = () => {
+  const { singleblog } = useSelector((state) => state.blog);
+  console.log(singleblog);
 
-    const {singleblog} = useSelector((state)=>state.blog)
-    console.log(singleblog)
+  const isoDate = singleblog.createdAt;
+  const dateObj = new Date(isoDate);
+  const formattedDate = dateObj.toLocaleDateString(); // Tarih: 17/10/2024
+  const formattedTime = dateObj.toLocaleTimeString();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        title={singleblog.userId.username}
+        subheader={`${formattedDate} ${formattedTime}`}
+      />
+      <CardMedia
+        component="img"
+        a
+        alt="green iguana"
+        height="140"
+        image={singleblog.image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {singleblog.title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {singleblog.content}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Like</Button>
+        <Button size="small">Yorum</Button>
+        <Button size="small">Görüntülenme</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default Detail
+export default Detail;
