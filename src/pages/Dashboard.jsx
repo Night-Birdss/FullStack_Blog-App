36ec183cbad36250d755cf563bc4530ff6e1b473
ozkan <ozkandnz09@gmail.com>
2024-10-12 12:43:14 +0300

    DashboardCard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,30 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import { Box } from "@mui/material";
+import React, { useEffect } from "react";
+import DashboardCard from "../components/blog/card";
+import axios from "axios";
 
-export default function ImgMediaCard() {
+const Dashboard = () => {
+  const {token} = useSelector((state)=>state)
+  const getİnfos = async () => {
+    try {
+      const { data } = await axios(`${process.env.REACT_APP_BASE_URL}/blogs/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //?Sayfa yüklendikten sonra firmaları getir
+  useEffect(() => {
+    getİnfos();
+  }, []);
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="Porsche"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Porsche
-        </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-        Once known just for its sports cars, Porsche now produces a wide variety of coupes, convertibles, SUVs and sedans.
-        </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary",}}>
-        Published Date : 24.01.2024 20:39:42
-        </Typography>
-      </CardContent>
-      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box>
-          <FavoriteIcon />
-          <ChatBubbleOutlineIcon />
-          <RemoveRedEyeIcon/>
-        </Box>
-        <Button size="small">Read More</Button>
-      </CardActions>
-    </Card>
+    <div>
+      <DashboardCard />
+    </div>
   );
-}
+};
+
+export default Dashboard;
