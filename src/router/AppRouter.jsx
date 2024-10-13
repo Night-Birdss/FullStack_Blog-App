b9011a6a4e70258acb7630629fc00128b6139136
asimov <seyranmustafa4@gmail.com>
2024-10-14 00:39:37 +0300

    NavBar and ToastNotify component added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -10,10 +10,12 @@ import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Detail from "../pages/Detail";
 import NewBlog from "../pages/NewBlog";
+import NavBar from "../components/NavBar";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
