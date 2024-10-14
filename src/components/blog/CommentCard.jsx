e102c52219721218464a9e5fe5421b416bf0e253
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-14 17:03:40 +0300

    yorumların gösterilmesi tamamlandı

--- src/components/blog/CommentCard.jsx
+++ src/components/blog/CommentCard.jsx
@@ -1,5 +1,19 @@
-const CommentCard = ({ comments }) => {
-  return <div>CommentCard</div>;
+import { useParams } from "react-router";
+
+const CommentCard = ({ comment }) => {
+  const { id } = useParams(); // URL'den id parametresini alıyoruz
+
+  if (comment.blogId !== id) {
+    return null; // Eşleşmezse hiçbir şey render etme
+  }
+
+  console.log(comment.blogId);
+  return (
+    <div>
+      <h4>{comment.userId.username}</h4> {/* Yazan kişi ismi */}
+      <p>{comment.comment}</p> {/* Yorum içeriği */}
+    </div>
+  );
 };
 
 export default CommentCard;
