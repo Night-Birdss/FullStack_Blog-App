cc50cee7ea563b7ce3b12b08121b0e08b310abd9
asimov <seyranmustafa4@gmail.com>
2024-10-14 13:51:01 +0300

    updated

--- src/components/blog/DashboardCard.jsx
+++ src/components/blog/DashboardCard.jsx
@@ -10,7 +10,7 @@ import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { Box } from "@mui/material";
 import useBlogCalls from "../../hooks/useBlogCalls";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function DashboardCard({ blog }) {
   const { getSingleBlog } = useBlogCalls();
