08188f9438b474ce14faf7043b1b49515fb3ff2e
asimov <seyranmustafa4@gmail.com>
2024-10-14 04:16:06 +0300

    updated

--- src/pages/NewBlog.jsx
+++ src/pages/NewBlog.jsx
@@ -48,7 +48,7 @@ const NewBlog = () => {
         sx={{
           position: "absolute",
           borderRadius: "3%",
-          top: "40%",
+          top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
           width: { xs: 280, md: 400 }, // Adjusts width for smaller and larger screens
