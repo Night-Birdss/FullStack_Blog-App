17700ddb52cbf2ebe54144a9e3cce83804bb9c0f
asimov <seyranmustafa4@gmail.com>
2024-10-17 02:54:39 +0300

    update

--- src/components/FooTer.jsx
+++ src/components/FooTer.jsx
@@ -1,30 +1,27 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+// import AppBar from "@mui/material/AppBar";
+// import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
 const FooTer = () => {
   return (
-    <AppBar 
-    // position="fixed"
-     sx={{ top: "auto", bottom: 0,}}
-     >
-      <Toolbar
+      <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
+          bgcolor:"primary.main",
+          color:"white"
         }}
       >
-        <Typography variant="body1" sx={{ }}>
+        <Typography variant="body1" >
           Developed by some Fullstack Developers
         </Typography>
-        
-        <Typography variant="body1" sx={{ display: "block" }}>
+        <Typography variant="body1" >
           Copyright &copy; {new Date().getFullYear()} nightBirds.co
         </Typography>
-      </Toolbar>
-    </AppBar>
+      </Box>
   );
 };
 
