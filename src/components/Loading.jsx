258f0c277b31d0fa431f33632e42ae3fdd573435
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-25 20:54:04 +0300

    loading footer ayarlandı

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -9,7 +9,7 @@ const Loading = () => {
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
-        height: "85.2vh",
+        height: "89.3vh",
       }}
     >
       <img src={LoadingGif} style={{ width: "200px" }} />
